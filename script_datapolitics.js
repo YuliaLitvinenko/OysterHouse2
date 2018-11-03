a57937a798be4f4c1283dd89c0bdb165cd0f17e4
YuliaLitvinenko <julia-litvinenko@yandex.ru>
2018-11-03 23:53:58 +0300

    PD button made more easy

--- script_datapolitics.js
+++
@@ -1,7 +0,0 @@
-function check() {
-	var submit = document.getElementsByName('submit')[0];
-		if (document.getElementById('politics').checked)
-			submit.disabled = '';
-		else
-			submit.disabled = 'disabled';
-	}
